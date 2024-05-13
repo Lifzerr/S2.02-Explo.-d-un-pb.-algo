be547b1a97147ca01a076fea8fdb44891b9852d6
Lifzerr <maxime.bourciez@gmail.com>
2024-05-13 14:14:48 +0200

    Ajout des fichiers pour part 3

---
+++ partie3/main.py
@@ -0,0 +1,41 @@
+import json
+import pandas as pd
+import numpy as np
+import os
+
+
+os.chdir("C:\\IUT\\Semestre 2\\S2.02 - Explo algorithmique d'un problème\\partie2")
+# os.chdir("E:\\Cours\\Semestre2\\S2.02\\S2.02-Explo.-d-un-pb.-algo\\partie2")
+
+# import dicsucc.json et dicsuccdist.json (--> dictionnaire)
+with open("dicsucc.json", "r") as fichier:
+    dicsucc = json.load(fichier)
+with open("dicsuccdist.json", "r") as fichier:
+    dicsuccdist = json.load(fichier)
+
+# import aretes.csv (--> dataframe) et transformation de lstpoints (chaîne-->liste)
+aretes = pd.read_table('aretes.csv', sep  =';', index_col= 0)
+
+for ind in aretes.index :
+    ls = aretes.loc[ind,'lstpoints'].replace(" ","").replace("]", "").replace("[", "").split(',')
+    lst = []
+    for val in ls :
+        lst.append(int(val))
+    aretes.at[ind,'lstpoints'] = lst
+
+
+# import sommets.csv, matrice_poids.csv (--> dataframe)
+sommets = pd.read_table('sommets.csv', sep  =';', index_col= 0)
+matrice_poids = pd.read_csv('matrice_poids.csv', sep = ';', index_col = 0)
+
+# transformation dataframe matrice des poids en tableau    
+tableau_poids = np.array(matrice_poids)
+
+# transformation matrice des poids en liste de listes
+liste_poids = [[None for j in range(len(tableau_poids))] for i in range(len(tableau_poids))]
+for i in range(len(tableau_poids)):
+    for j in range(len(tableau_poids)):
+        liste_poids[i][j]  = tableau_poids[i,j]
+
+
+del fichier, i, j, val, ls, lst, ind 
